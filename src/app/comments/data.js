c31f43afec13f406cb185f8cdfd77f1f80bb8e03
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-11-20 00:34:41 +0800

    adding get and post request. part of practice

---
+++ src/app/comments/data.js
@@ -0,0 +1,18 @@
+export const comments = [
+  {
+    id: 1,
+    text: "First comment",
+  },
+  {
+    id: 2,
+    text: "Second comment",
+  },
+  {
+    id: 3,
+    text: "Third comment",
+  },
+  {
+    id: 4,
+    text: "Forth comment",
+  },
+];
