811e48a59d0b0e7189dbd4a39c9343acc8c4d4fe
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-11-28 23:51:09 +0800

    yezzer

--- src/app/client-route/page.js
+++ src/app/client-route/page.js
@@ -1,15 +1,34 @@
 "use client"
-
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { serverSideFunction } from "../utils/server-utils";
+
 export default function ClientRoute() {
     console.log("Client route rendered");
     
 
   const result = serverSideFunction();
+  const settings = {
+    dots: true,
+  };
   return (
-    <>
-        <h1>ClientRoutePage</h1>
-        <p>{result}</p>
-    </>
-  )
+    <div className="image-slider-container">
+      <Slider {...settings}>
+        <div>
+          <img className='object-cover h-48 w-96' src="https://picsum.photos/123/300" />
+        </div>
+        <div>
+          <img className='object-cover h-48 w-96' src="https://picsum.photos/112/300" />
+        </div>
+        <div>
+          <img className='object-cover h-48 w-96' src="https://picsum.photos/432/300" />
+        </div>
+        <div>
+          <img className='object-cover h-48 w-96' src="https://picsum.photos/756/300" />
+        </div>
+      </Slider>
+    </div>
+  );
 }
